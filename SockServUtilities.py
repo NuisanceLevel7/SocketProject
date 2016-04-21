0f3d9b3b4aed1544ef39f5bb8a07328fbbb3fed7
Victor Engle <victor.engle@gmail.com>
2016-04-21 16:11:13 -0500

    moved the connection handler into the utility module and dod some cleanup

--- SockServUtilities.py
+++ SockServUtilities.py
@@ -3,6 +3,62 @@
 import re,os,time,datetime,subprocess,sys
 import os.path
 
+class SocketConnectionServer:
+
+  def __init__(self):
+    self.status = ''    
+  
+  #Function for handling connections. This will be used to create threads
+  def ConnectionHandler(self,conn,params):
+  
+    logger = Files()
+
+
+    
+    Dates = DateString()
+    # Initial connect message to the client
+    connection_num = params[0]
+    remotehost = params[1]
+    client_log = params[2]
+  
+    pid = os.getpid()
+  
+    logger.write_log(client_log, "PID " + str(pid) + " Handling Conn# " + connection_num + "\n")
+    logger.write_log(client_log, "Conn# " + str(connection_num) + " talking to remote host, " + str(remotehost) + "\n")
+
+
+    conn.send('Connection established. Ready to receive')
+    connection_num = params[0]
+  
+    file_open = False	
+    # Handle client file transfer
+    while True:
+    
+      #Receiving from client
+      data = conn.recv(1024)
+      if not data:
+        reply = 'Disconnecting from IP addr ' + remotehost + '...\n\n'
+        conn.sendall(reply)
+        print "Sending disconnect notice to Client"
+        conn.close()
+        print "\n\nGoodbye... " + data + "\n\n"
+        break
+      elif file_open:
+        bfh.write(data)
+      elif 'SOCK_CLIENT_SENDING_FILE' in data.upper():
+        fields = data.split()
+        filename = "incoming/" + fields[1]
+        filename = filename.strip()
+        bfh = open(filename,'wb')
+        file_open = True
+        print "Sending: Prepared to receive"
+        conn.send('Prepared to receive ' + fields[1])
+    #came out of loop
+  
+    bfh.close()
+    conn.close()
+
+
 class DateString:
 
   def __init__(self):
--- sockserv.py
+++ sockserv.py
@@ -9,16 +9,17 @@ from thread import *
 import re,os,time,datetime,subprocess,socket,sys
 from SockServUtilities import DateString
 from SockServUtilities import Files
+from SockServUtilities import SocketConnectionServer
 from optparse import OptionParser
 
+# Parse command line options...
 parser = OptionParser()
-
-
 parser.add_option("-p", "--port", dest="port",
                   help="Port on remote host for connection")
-                  
-
 (options, args) = parser.parse_args()                  
+
+
+# Set a few variables...
 HOST = ''
 PORT = 8767
 WORKERS = 10
@@ -26,76 +27,30 @@ sockserv_log = 'sockserv.log'
 client_log = 'connection_handler.log'
 pid = os.getpid()
 logger = Files()
-
 if options.port:
-  port = int(options.port)
+  PORT = int(options.port)
 
 logger.write_log(sockserv_log, "Socket Server Starting Up. PID=" + str(pid) + "\n")
-
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 print 'Socket created'
 
-#Bind socket to local host and port
+# Bind socket to local host and port, but catch exceptions
 try:
   s.bind((HOST, PORT))
 except socket.error as msg:
-  print 'Bind failed. Error Code : ' + str(msg[0]) + ' Message ' + msg[1]
-  sys.exit()
+  print 'Unable to bind to port: ' + str(PORT) + "\n\nError message from the OS...\n\n" + str(msg[0]) + ' Message ' + msg[1] + "\n\nExiting..."
+  sys.exit(10)
 
 print 'Socket bind complete'
 logger.write_log(sockserv_log, "Socket Server Bind Okay, Listening Socket open on Port-" + str(PORT) + "\n")
-#Start listening on socket.
+# Start listening on socket.
 s.listen(WORKERS)
 print 'Socket now listening on port, ' + str(PORT)
 
-#Function for handling connections. This will be used to create threads
-def ConnectionHandler(conn,params):
-
-  global welcome
-  Dates = DateString()
-  # Initial connect message to the client
-  connection_num = params[0]
-  remotehost = params[1]
-  
-  pid = os.getpid()
-  
-  logger.write_log(client_log, "PID " + str(pid) + " Handling Conn# " + connection_num + "\n")
-  logger.write_log(client_log, "Conn# " + str(connection_num) + " talking to remote host, " + str(remotehost) + "\n")
-
-
-  conn.send('Connection established. Ready to receive')
-  connection_num = params[0]
-  
-  file_open = False	
-  # Handle client file transfer
-  while True:
-    
-    #Receiving from client
-    data = conn.recv(1024)
-    if not data:
-      reply = 'Disconnecting from IP addr ' + remotehost + '...\n\n'
-      conn.sendall(reply)
-      print "Sending disconnect notice to Client"
-      conn.close()
-      print "\n\nGoodbye... " + data + "\n\n"
-      break
-    elif file_open:
-	  bfh.write(data)
-    elif 'SOCK_CLIENT_SENDING_FILE' in data.upper():
-      fields = data.split()
-      filename = "incoming/" + fields[1]
-      filename = filename.strip()
-      bfh = open(filename,'wb')
-      file_open = True
-      print "Sending: Prepared to receive"
-      conn.send('Prepared to receive ' + fields[1])
-  #came out of loop
-  
-  bfh.close()
-  conn.close()
-
-#now keep talking with the client
+# Now listen for client connection on the bind port and pass them off workers.
+# We create one worker object and use the threads library to run parallel instances
+worker = SocketConnectionServer()
 conn_count = 0
 while 1:
   #wait to accept a connection - blocking call
@@ -105,9 +60,12 @@ while 1:
   #
   # This is a tuple of arguments to pass to client connection handler
   #
-  client_handler_args = str(conn_count), str(addr[0])
-  #start new thread takes 1st argument as a function name to be run, second is the tuple of arguments to the function.
-  start_new_thread(ConnectionHandler ,(conn,client_handler_args))
+  client_handler_args = str(conn_count), str(addr[0]), client_log
+  #
+  # Start new thread takes 1st argument as a function name to be run, second is a tuple of arguments to the function.
+  # In this case, the function is a a method from the class imported from SocketConnectionServer
+  # and the tuple is in this format: tuple(connection_object, tuple(connection#, remote_IP, client_log))
+  start_new_thread(worker.ConnectionHandler ,(conn,client_handler_args))
 
 s.close()
 
